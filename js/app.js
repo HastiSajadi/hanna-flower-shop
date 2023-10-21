0666ae56b582e9056f893f97be4d66f7ad53fc4e
hasti sajadi <hastiisajadii@gmail.com>
2023-10-21 23:15:01 +0330

    responsive discount section

--- js/app.js
+++ js/app.js
@@ -23,7 +23,7 @@ window.onscroll = ()=>{
 
 function like(x) {
     x.classList.toggle("fa-solid");
-  }
+}
 
 
 
@@ -38,3 +38,30 @@ function like(x) {
 //   }
 
 
+// deal countDown
+
+let countDate = new Date('october 30 , 2023 02:00:00').getTime();
+
+function countDown(){
+
+    let now =new Date().getTime();
+    gap = countDate - now;
+    let second = 1000;
+    let minute = second * 60;
+    let hour = minute * 60;
+    let day = hour * 24;
+
+    let d = Math.floor(gap / (day));
+    let h = Math.floor((gap) % (day) / (hour));
+    let m = Math.floor((gap) % (hour) / (minute));
+    let s = Math.floor((gap) % (minute) / (second));
+    
+    document.getElementById('day').innerText = d;
+    document.getElementById('hour').innerText = h;
+    document.getElementById('minute').innerText = m;
+    document.getElementById('second').innerText = s;
+}
+
+setInterval(function(){
+    countDown()
+},1000);
